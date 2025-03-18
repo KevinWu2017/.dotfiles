26cc7bdd6f38d0902620b5a1dc255c84cd3f02b2
cpwu <cpwu2017@126.com>
2025-03-18 18:38:06 +0800

    update the modification for zsh & bash

--- pkg-management/brew-install.sh
+++ pkg-management/brew-install.sh
@@ -15,5 +15,5 @@ rm -rf brew-install
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-test -r ~/.bashrc && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
-test -r ~/.zshrc && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+test -r ~/.bashrc && echo -e "\neval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+test -r ~/.zshrc && echo -e "\neval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
